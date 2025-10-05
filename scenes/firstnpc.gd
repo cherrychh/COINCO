e66d83d3f32130e583eac49c6dd6fce7d743bfc3
cherrychh <cherrychtao@gmail.com>
2025-10-05 20:49:49 +0300

    First NPC, NPC animation, Dialogue
    Things added:
    - First NPC
       - NPC animation
       - NPC dialogue
          - Had to scrap a portion of it cuz found out that using dialogic was easier

---
+++ scenes/firstnpc.gd
@@ -0,0 +1,38 @@
+extends CharacterBody2D
+
+var current_state = IDLE
+var is_chatting = false
+var player
+var player_in_chatting_zone = false
+
+enum {
+	IDLE
+}
+
+func _process(delta):
+	if Input.is_action_just_pressed("chat"):
+		print("Chatting!!!")
+		#run_dialogue("AutumnChat")
+		is_chatting = true
+	if current_state == 0:
+		$AnimatedSprite2D.play("idle")
+	
+
+		
+func choose(array):
+	array.shuffle()
+	return array.front()
+
+func _on_timer_timeout() -> void:
+	$Timer.wait_time = choose([1,2])
+	current_state = IDLE
+
+
+func _on_chat_detection_body_entered(body: Node2D) -> void:
+	if body.has_method("player"):
+		player_in_chatting_zone = true
+
+
+func _on_chat_detection_body_exited(body: Node2D) -> void:
+	if body.has_method("player"):
+		player_in_chatting_zone = false
